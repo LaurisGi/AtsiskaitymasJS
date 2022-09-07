f9dfdae89ee515a4e8d0de4947f0d3132cf7239a
LaurisGi <laurynas.gipas@gmail.com>
2022-09-07 12:55:59 +0300

    6 task done

--- Task 6/script.js
+++ Task 6/script.js
@@ -19,3 +19,12 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+const getUserAverageAge = (arr) => {
+  const ageArr = arr.reduce((total, next) => total + next.age, 0) / arr.length;
+  return ageArr
+};
+const getUsersNames = (arr) => {
+  const userNames = arr.map((arr)=> arr.name);
+  return userNames
+};
